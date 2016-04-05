82ad10b3bab60ea00fd3d78ec7d9bf482b9c24ef
Blackhat Digital <josh@blackhatdigital.com.au>
2016-04-05 05:04:02 -0700

    Add avatar to user profile

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,12 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
+ 	has_attached_file	:avatar, styles: { medium: "236x236#", thumb: "100x100#" }, default_url: "missing.png",
+						:storage => :dropbox,
+   						:dropbox_credentials => Rails.root.join("config/dropbox.yml")
+
+   	validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\Z/
+
  
   	devise	:database_authenticatable, :registerable,
         	:recoverable, :rememberable, :trackable, :validatable
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160405042627) do
+ActiveRecord::Schema.define(version: 20160405114623) do
 
   create_table "orders", force: :cascade do |t|
     t.string   "address"
@@ -51,6 +51,10 @@ ActiveRecord::Schema.define(version: 20160405042627) do
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
     t.string   "name"
+    t.string   "avatar_file_name"
+    t.string   "avatar_content_type"
+    t.integer  "avatar_file_size"
+    t.datetime "avatar_updated_at"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
